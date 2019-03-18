bf27bb0ad650daaa2a6131b90e5d9cf9e2a27e80
microtest10 <microtest10@gmail.com>
2019-03-18 12:05:32 +0530

    Adding c file

---
+++ clarg.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int  main(int argc,char *argv[]) 
+{
+    if (argc>2)
+    {
+    for (int i=2;i<=argc;i++)
+    printf("%s",argv[i]);
+    }
+    if (argc==2)
+    printf("%s",argv[2]);
+    }
